22b9ba6414012d1e217980ced74b17e28fcec6ed
prayash24 <prayashnaik24@gmail.com>
2025-09-25 22:08:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-const  heading = React.createElement("h1", {id: "heading"}, "hello world from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("h1", { id: "heading" }, "hello world");
+
+// createRoot API (React 18+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(heading);
