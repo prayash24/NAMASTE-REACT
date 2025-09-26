edbd446445117ab93cf2dfce4f8f8b2ea1970add
prayash24 <prayashnaik24@gmail.com>
2025-09-27 00:14:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,4 +5,4 @@ const heading = React.createElement("h1", { id: "heading" }, "hello world");
 
 // createRoot API (React 18+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading); 
