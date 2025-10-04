79fb9d920d61473969625ef5954c9b5e06921791
prayash24 <prayashnaik24@gmail.com>
2025-10-04 22:58:33 +0530

    Create NAMASTE REACT.6bd02f5a.js

---
+++ dist/NAMASTE REACT.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=NAMASTE REACT.6bd02f5a.js.map
