302a3cad9d95b0cdf8f7056b8b3d28a15bcc6840
prayash24 <prayashnaik24@gmail.com>
2025-10-25 00:16:42 +0530

    Update RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,37 +1,49 @@
 import { CDN_URL } from '../utils/constants';
+import { FiClock } from 'react-icons/fi';
+import { AiOutlineStar } from 'react-icons/ai';
 
 const RestaurantCard = (props) => {
-    const { resData } = props;
+  const { resData } = props;
 
-    const {
-        cloudinaryImageId,
-        name,
-        cuisines,
-        avgRating,
-        costForTwo,
-        deliveryTime,
-    } = resData?.data;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
 
-    return (
-        <div
-            className="res-card"
-            style={{
-                backgroundColor: '#f0f0f0',
-            }}
-        >
-            <img
-                className="res-logo"
-                src={CDN_URL + cloudinaryImageId}
-                alt="Biryani"
-            />
+  return (
+    <div className="res-card">
+      <div className="res-img">
+        <img
+          className="res-logo"
+          src={CDN_URL + cloudinaryImageId}
+          alt="Biryani"
+        />
+      </div>
 
-            <h3>{name}</h3>
-            <h4>{cuisines.join(', ')}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4>₹{costForTwo / 100} FOR TWO</h4>
-            <h4>{deliveryTime} minutes</h4>
-        </div>
-    );
+      <div className="res-card-content">
+        <h3>{name}</h3>
+        <hr />
+        <em>{cuisines.join(', ')}</em>
+        <h4 className="avg-rating">
+          <span className="icons">
+            <AiOutlineStar />
+          </span>
+          <span>{avgRating} stars</span>
+        </h4>
+        <h4 className="item-price">₹ {costForTwo / 100} FOR TWO</h4>
+        <h4 className="time">
+          <span className="icons">
+            <FiClock />
+          </span>
+          <span> {deliveryTime} minutes</span>
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantCard;
